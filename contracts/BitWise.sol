5dc88ae2d3c74ca70dc3554c1587d192729f3f50
world324digital <70345255+world324digital@users.noreply.github.com>
2022-01-16 20:20:34 +0300

    create bitwise and add func countbitset

---
+++ contracts/BitWise.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.5.0;
+
+contract BitWise {
+    // count the number of bit set in data.  i.e. data = 7, result = 3
+    function countBitSet(uint8 data) public pure returns (uint8 result) {
+        }
+    }
+}
